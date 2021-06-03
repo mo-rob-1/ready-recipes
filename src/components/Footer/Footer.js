386f29303e724354a800a98f7f2bf84f3d2833a7
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-06-03 18:25:36 +0100

    add footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Wrapper } from "./Footer.styles"
+
+const Footer = () => {
+  return (
+    <Wrapper>
+      <small>© 2021 | Ready Recipes</small>
+    </Wrapper>
+  )
+}
+
+export default Footer
---
+++ src/components/Footer/Footer.styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components"
+
+export const Wrapper = styled.footer`
+  text-align: center;
+  font-family: "Poppins", sans-serif;
+`
