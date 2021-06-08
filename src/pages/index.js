722ee2a5ceb0378e315679f7d654384d7d235f3b
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-06-08 18:25:28 +0100

    add about component to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import * as React from "react"
 import Seo from "../components/SEO/SEO"
 import Layout from "../components/Layout/Layout"
 import Hero from "../components/Hero/Hero"
+import About from "../components/About/About"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <Hero />
+    <About />
   </Layout>
 )
 
