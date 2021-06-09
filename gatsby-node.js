41f97ecf8cfa567b8a7c420fd545f939f6ae0d6f
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-06-09 22:28:09 +0100

    configured to generate post and recipe pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,52 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require(`path`)
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions
+
+  const queryResult = await graphql(`
+    {
+      postQuery: allWpPost(sort: { fields: date }) {
+        nodes {
+          title
+          excerpt
+          content
+          slug
+        }
+      }
+      recipeQuery: allWpRecipe(sort: { fields: date }) {
+        nodes {
+          title
+          excerpt
+          content
+          slug
+        }
+      }
+    }
+  `)
+  if (queryResult.errors) {
+    reporter.panic("error loading events", queryResult.errors)
+    return
+  }
+
+  const recipes = queryResult.data.recipeQuery.nodes
+  recipes.forEach(node => {
+    createPage({
+      path: `/recipes/${node.slug}`,
+      component: path.resolve(`./src/templates/recipe-post.js`),
+      context: {
+        slug: node.slug,
+      },
+    })
+  })
+
+  const posts = queryResult.data.postQuery.nodes
+  posts.forEach(node => {
+    createPage({
+      path: `/blog/${node.slug}`,
+      component: path.resolve(`./src/templates/post.js`),
+      context: {
+        slug: node.slug,
+      },
+    })
+  })
+}
