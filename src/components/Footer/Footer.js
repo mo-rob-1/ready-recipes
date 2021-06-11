f2f59bdcff0b2fbbdf6d2686c3fd64d29d4ffd42
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-06-11 19:11:06 +0100

    update footer text

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,10 @@ import { Wrapper } from "./Footer.styles"
 const Footer = () => {
   return (
     <Wrapper>
-      <small>© 2021 | Ready Recipes</small>
+      <small>
+        © 2021 | Ready Recipes - Website built by Mo. using GatsbyJS and
+        WordPress
+      </small>
     </Wrapper>
   )
 }
