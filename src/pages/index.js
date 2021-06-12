ac4b064c9f01ffda5a0dd0d65e523079883e96db
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-06-12 19:59:26 +0100

    add components to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,20 @@ import Seo from "../components/SEO/SEO"
 import Layout from "../components/Layout/Layout"
 import Hero from "../components/Hero/Hero"
 import About from "../components/About/About"
+import FeaturedRecipes from "../components/FeaturedRecipes/FeaturedRecipes"
+import FeaturedPost from "../components/FeaturedPost/FeaturedPost"
+import HomepagePosts from "../components/HomepagePosts/HomepagePosts"
+import SubscribeSection from "../components/SubscribeSection/SubscribeSection"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <Hero />
     <About />
+    <FeaturedRecipes />
+    <FeaturedPost />
+    <HomepagePosts />
+    <SubscribeSection />
   </Layout>
 )
 
