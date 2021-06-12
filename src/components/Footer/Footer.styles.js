7d1b55eb00dd2749db4a681fdc4b65408a5e37eb
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-06-12 19:47:49 +0100

    style footer links

--- src/components/Footer/Footer.styles.js
+++ src/components/Footer/Footer.styles.js
@@ -1,6 +1,17 @@
 import styled from "styled-components"
+import Link from "gatsby-link"
 
 export const Wrapper = styled.footer`
+  padding: 1rem;
   text-align: center;
+`
+export const Text = styled.small`
   font-family: "Karla", sans-serif;
+  line-height: 1.5;
+  color: #8f8f8f;
+`
+
+export const StyledLink = styled(Link)`
+  text-decoration: underline;
+  color: #8f8f8f;
 `
