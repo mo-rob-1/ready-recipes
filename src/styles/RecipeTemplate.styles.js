4b3d1a638e4ddb305dd7756730acdb39d3b78e53
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-06-15 19:06:48 +0100

    update styling

--- src/styles/RecipeTemplate.styles.js
+++ src/styles/RecipeTemplate.styles.js
@@ -14,11 +14,25 @@ export const Title = styled.h1`
   text-align: center;
   line-height: 1.5;
 `
-export const Excerpt = styled.div`
+export const Content = styled.div`
   line-height: 1.6;
   font-family: "Karla", sans-serif;
   font-size: 0.9rem;
   margin-bottom: 2rem;
+  padding: 0rem 1rem;
+`
+export const Ingredients = styled.div`
+  line-height: 1.6;
+  font-family: "Karla", sans-serif;
+  font-size: 0.9rem;
+  margin-bottom: 2rem;
+  padding: 0rem 1rem;
+  margin-top: 1rem;
+`
+export const Preparation = styled.div`
+  line-height: 1.6;
+  font-family: "Karla", sans-serif;
+  font-size: 0.9rem;
   padding: 0rem 1rem;
 `
 export const StyledLink = styled(Link)`
@@ -31,4 +45,53 @@ export const Breadcrumb = styled.h4`
   margin-bottom: 0;
   line-height: 1.5;
   margin-top: 2rem;
+`
+export const RecipeImg = styled.img`
+  width: 100%;
+`
+export const RecipeOverviewWrapper = styled.div`
+  padding: 1rem;
+`
+export const ImgWrapper = styled.div`
+  display: block;
+`
+export const OverviewTextWrapper = styled.div`
+  display: block;
+`
+export const OverviewTitle = styled.h1`
+  font-weight: lighter;
+  font-size: 3rem;
+  line-height: 1.3;
+`
+export const RecipeSummary = styled.p`
+  font-family: "Karla", sans-serif;
+  line-height: 1.6;
+  font-size: 1.2rem;
+`
+export const AdditionalInfoWrapper = styled.div`
+  display: block;
+`
+export const CookingTime = styled.p`
+  display: block;
+  margin-top: 1.8rem;
+  margin-bottom: 1.8rem;
+`
+export const Serves = styled.p`
+  display: block;
+`
+export const CookingWrapper = styled.div`
+  display: block;
+`
+export const CookingTitle = styled.h5`
+  padding: 0.6rem;
+  font-family: "Karla", sans-serif;
+  border: solid 1px #000;
+  display: inline-block;
+`
+export const CookingTitleWrapper = styled.div`
+  padding-left: 1rem;
+  font-weight: lighter;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+  font-size: 0.8rem;
 `
