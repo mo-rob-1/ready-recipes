3f1b84ed61b052e1d9d3b253df692fad27d33f95
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-06-15 19:07:57 +0100

    update template markup

--- src/templates/recipe-post.js
+++ src/templates/recipe-post.js
@@ -1,13 +1,29 @@
 import React from "react"
 import Layout from "../components/Layout/Layout"
 import { graphql } from "gatsby"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
   TitleBanner,
   Breadcrumb,
   Title,
-  Excerpt,
+  Content,
+  RecipeImg,
   StyledLink,
+  RecipeOverviewWrapper,
+  ImgWrapper,
+  OverviewTextWrapper,
+  OverviewTitle,
+  RecipeSummary,
+  AdditionalInfoWrapper,
+  Serves,
+  CookingTime,
+  CookingTitle,
+  CookingTitleWrapper,
+  Ingredients,
+  Preparation,
+  CookingWrapper,
 } from "../styles/RecipeTemplate.styles"
+import { faClock, faUser } from "@fortawesome/free-solid-svg-icons"
 
 export default function RecipePost({ data }) {
   const post = data.allWpRecipe.nodes[0]
@@ -15,14 +31,63 @@ export default function RecipePost({ data }) {
   return (
     <Layout>
       <div>
-        <TitleBanner>
+        {/* <TitleBanner>
           <Title>{post.title}</Title>
-        </TitleBanner>
+        </TitleBanner> */}
         <Breadcrumb>
           <StyledLink to="/">Home</StyledLink> /{" "}
           <StyledLink to="/recipes">Recipes</StyledLink> / {post.title}
         </Breadcrumb>
-        <Excerpt dangerouslySetInnerHTML={{ __html: post.content }} />
+
+        <RecipeOverviewWrapper>
+          <ImgWrapper>
+            <RecipeImg src={post.ingredients.image.localFile.url} />
+          </ImgWrapper>
+
+          <OverviewTextWrapper>
+            <OverviewTitle>{post.title}</OverviewTitle>
+            <RecipeSummary>{post.ingredients.recipeOverview}</RecipeSummary>
+
+            <AdditionalInfoWrapper>
+              <CookingTime>
+                {" "}
+                <FontAwesomeIcon
+                  icon={faClock}
+                  style={{ fontSize: "1.6rem", marginRight: "12px" }}
+                />
+                {post.ingredients.cookingTime} minutes
+              </CookingTime>
+              <Serves>
+                {" "}
+                <FontAwesomeIcon
+                  icon={faUser}
+                  style={{ fontSize: "1.6rem", marginRight: "12px" }}
+                />
+                Serves: {post.ingredients.serves}
+              </Serves>
+            </AdditionalInfoWrapper>
+          </OverviewTextWrapper>
+        </RecipeOverviewWrapper>
+
+        <Content dangerouslySetInnerHTML={{ __html: post.content }} />
+
+        <CookingWrapper>
+          <CookingTitleWrapper>
+            <CookingTitle>Ingredients</CookingTitle>
+          </CookingTitleWrapper>
+          <Ingredients
+            dangerouslySetInnerHTML={{ __html: post.ingredients.ingredients }}
+          />
+        </CookingWrapper>
+
+        <CookingWrapper>
+          <CookingTitleWrapper>
+            <CookingTitle>Preparation</CookingTitle>
+          </CookingTitleWrapper>
+          <Preparation
+            dangerouslySetInnerHTML={{ __html: post.ingredients.preparation }}
+          />
+        </CookingWrapper>
       </div>
     </Layout>
   )
@@ -33,6 +98,18 @@ export const query = graphql`
       nodes {
         title
         content
+        ingredients {
+          recipeOverview
+          ingredients
+          preparation
+          image {
+            localFile {
+              url
+            }
+          }
+          cookingTime
+          serves
+        }
       }
     }
   }
