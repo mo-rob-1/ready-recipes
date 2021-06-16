d223ea3a649a9f99b75b624484a48cc15674c4c3
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-06-16 20:23:50 +0100

    update image custom hook query

--- src/hooks/useImageQuery.js
+++ src/hooks/useImageQuery.js
@@ -3,7 +3,7 @@ import { useStaticQuery, graphql } from "gatsby"
 export const useImageQuery = () => {
   const data = useStaticQuery(graphql`
     query ImageQuery {
-      cookingImage: file(relativePath: { eq: "cooking.jpg" }) {
+      aboutusImage: file(relativePath: { eq: "about-us-image.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 1920, quality: 90) {
             ...GatsbyImageSharpFluid
