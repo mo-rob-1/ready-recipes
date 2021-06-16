527d2a1d0b87bc998a6761517f3c0b4a258199f1
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-06-16 20:24:53 +0100

    add styling

---
+++ src/styles/About.styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components"
+
+export const IntroWrapper = styled.div`
+  background-color: #f5eaea;
+  padding: 3rem 1rem;
+`
+
+export const IntroImgWrapper = styled.div`
+  display: block;
+`
+
+export const IntroTextWrapper = styled.div`
+  display: block;
+`
+export const IntroTitle = styled.h1`
+  font-size: 3rem;
+  line-height: 1.4;
+  font-weight: lighter;
+`
+export const IntroDesc = styled.p`
+  font-family: "Karla", sans-serif;
+  line-height: 1.6;
+`
+
+export const AboutContent = styled.div`
+  font-family: "Karla", sans-serif;
+  line-height: 1.5;
+  padding: 3rem 1rem;
+`
---
+++ src/styles/Contact.styles.js
@@ -0,0 +1,59 @@
+import styled from "styled-components"
+
+export const ContactWrapper = styled.div`
+  padding: 3rem 1rem;
+  background-color: #f5eaea;
+`
+export const ContactInfoWrapper = styled.div`
+  display: block;
+`
+export const InnerWrapper = styled.div`
+  display: block;
+`
+export const Heading = styled.div`
+  line-height: 1.5;
+  font-size: 1.8rem;
+  margin-bottom: 2rem;
+`
+export const Desc = styled.div`
+  font-family: "Karla", sans-serif;
+  line-height: 1.5;
+  font-size: 0.9rem;
+`
+export const AddressWrapper = styled.div`
+  font-family: "Karla", sans-serif;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+`
+export const NumberAndEmailWrapper = styled.div`
+  font-family: "Karla", sans-serif;
+  margin-top: 2rem;
+  font-size: 0.9rem;
+`
+export const IconsWrapper = styled.div`
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+`
+export const FormWrapper = styled.div`
+  display: block;
+`
+export const Form = styled.form`
+  display: block;
+`
+export const FormItemWrapper = styled.div`
+  display: block;
+  margin-bottom: 1.8rem;
+`
+export const Label = styled.label`
+  font-family: "Karla", sans-serif;
+`
+export const FormInput = styled.input`
+  width: 100%;
+  padding: 0.5rem;
+  margin-top: 0.4rem;
+`
+export const TextArea = styled.textarea`
+  display: block;
+  width: 100%;
+  margin-top: 0.4rem;
+`
