0fc7a5ad095537948dc1633d3f90c0f7b7390565
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-06-20 20:17:25 +0100

    delete react state

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,12 @@
-import React, { useState } from "react"
+import React from "react"
 import { GlobalStyles, Container } from "./Layout.styles"
 import Header from "../Header/Header"
-import HamburgerIcon from "../HamburgerIcon/HamburgerIcon"
 import Footer from "../Footer/Footer"
 
 const Layout = ({ children }) => {
-  const [menuOpen, setMenuOpen] = useState(false)
-
-  const handleViewMenu = () => setMenuOpen(prev => !prev)
-
   return (
     <>
       <GlobalStyles />
-      {/* <HamburgerIcon handleViewMenu={handleViewMenu} /> */}
       <Header />
       <Container>{children}</Container>
       <Footer />
