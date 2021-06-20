b936d3de1b468dbc8b8cbea1445440fafcd36f6b
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-06-20 20:18:05 +0100

    delete comment

--- src/components/Layout/Layout.styles.js
+++ src/components/Layout/Layout.styles.js
@@ -1,8 +1,6 @@
 import styled, { createGlobalStyle } from "styled-components"
 
 export const Container = styled.main`
-  /* margin: 0 auto;
-  max-width: 1300px; */
   font-family: "Baskervville", serif;
 `
 
